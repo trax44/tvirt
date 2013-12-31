ad758dbdd9a1dbf0ce11f6a0f20ffcd1d8e51a2e
trax <o.givernaud@gmail.com>
2013-12-31 23:28:03 +0100

    cleanning

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -19,22 +19,6 @@ int main(int argc, char *argv[]) {
 
   }
 
-  // Return<tvirt::daemon::Virt::List> rList = virt.getListAllVm();
-
-  // if (!rList.success){
-  //   std::cerr << "FAIL" << std::endl;
-  //   return -1;
-  // }
-
-  // std::cout << rList.data.size() << " domain found" << std::endl;
-
-  // for (tvirt::daemon::Virt::List::const_iterator 
-  //        it = rList.data.begin() , end = rList.data.end();
-  //      it != end ; ++it){
-
-  //   std::cout << "vm> " << it->getName() << std::endl;
-    
-  // }
 
   return 0;
 }
