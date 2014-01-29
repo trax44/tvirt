fc21d7e1837c82ff6753609815c795a44fca171a
trax <o.givernaud@gmail.com>
2014-01-29 02:47:29 +0100

    stash HyervisorConnect segfault on delete

--- master/src/web/HypervisorConnect.hpp
+++ master/src/web/HypervisorConnect.hpp
@@ -2,12 +2,17 @@
 #define TVIRT_MASTER_WEB_HYPERVISORCONNECT_H_
 
 
-#include <Wt/WContainerWidget>
+#include <Wt/WDialog>
 #include <Wt/WLineEdit>
 #include <Wt/WPushButton>
 #include <Wt/WText>
 #include <Wt/WVBoxLayout>
 #include <Wt/WHBoxLayout>
+#include <Wt/WIntValidator>
+#include <Wt/WSignal>
+
+#include <string>
+#include <stdint.h>
 
 namespace tvirt {
 
@@ -15,14 +20,20 @@ namespace master {
 
 namespace web {
 
-class HypervisorConnect : public Wt::WContainerWidget {
+class HypervisorConnect : public Wt::WDialog {
 private:
-  Wt::WLineEdit     wtAddress;
-  Wt::WLineEdit     wtPort;
+  Wt::WLineEdit        wtAddress;
+  Wt::WLineEdit        wtPort;
+  Wt::WIntValidator    validator;
+
   void connect();
+  Wt::Signal<std::string , uint16_t> done_;
   
+  void validate();
+
 public:
-  HypervisorConnect(Wt::WContainerWidget *parent);
+  HypervisorConnect();
+  Wt::Signal<std::string , uint16_t>& res() { return done_; }
 };
 
 
